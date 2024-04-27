b256dd57d16ef0b391fb381bb889ca3398718e4d
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-04-26 23:14:38 -0700

    APP cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import { useLocation } from "react-router-dom" // Import useLocation here
+import { useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { About, Contact, Home, Projects } from "./pages"
 import SpaceBackground from "./hooks/SpaceBackground"
 
-// Create a new component for handling routes and applying your mainClass logic
 const MainContent = () => {
-    const location = useLocation() // useLocation is called inside a component which will be rendered inside Router
+    const location = useLocation()
 
     let mainClass = location.pathname === '/contact' ? 'h-full' : ''
     if (['/'].includes(location.pathname)) {
@@ -28,7 +27,6 @@ const MainContent = () => {
     )
 }
 
-// Adjust the App component to only include Router and render MainContent inside it
 const App = () => {
     return (
         <Router>
