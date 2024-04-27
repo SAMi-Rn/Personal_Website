f915840161f29abbae5ac78c58cdaeb2ad8a74e1
SAMi-Rn <sami_roudgarian@yahoo.com>
2024-04-26 23:18:31 -0700

    Satellite cleanup

--- src/models/Satellite.jsx
+++ src/models/Satellite.jsx
@@ -9,14 +9,13 @@ const Satellite = ({ onLoad }) => {
     const { scene, animations } = useGLTF(satelliteScene)
     const { actions } = useAnimations(animations, sattelliteRef)
     const { camera } = useThree()
-    const [modelLoaded, setModelLoaded] = useState(false) // State to track if the model has loaded
+    const [modelLoaded, setModelLoaded] = useState(false)
 
     useEffect(() => {
-        // Check if the model has been loaded
         if (scene && animations) {
             setModelLoaded(true)
             if (onLoad) {
-                onLoad() // Call the onLoad callback if provided
+                onLoad()
             }
         }
     }, [scene, animations, onLoad])
@@ -26,22 +25,18 @@ const Satellite = ({ onLoad }) => {
 
     useFrame(({ clock }) => {
         const time = clock.getElapsedTime()
-        const orbitSpeed = 0.2 // Lower this value to slow down the speed of the orbit
+        const orbitSpeed = 0.2
         const radius = window.innerWidth < 768 ? 15 : 25
         const planetPosition = [0, 0, -43.4]
 
         if (sattelliteRef.current) {
-            // Apply the speed factor to time to slow down the orbit
             const angle = orbitSpeed * time
 
-            // Calculate the new X and Z positions for the satellite to orbit around the planet's position
-            sattelliteRef.current.position.x = Math.cos(angle) * radius + planetPosition[0]  // X-axis
-            sattelliteRef.current.position.y = planetPosition[1] // Y-axis
-            sattelliteRef.current.position.z = Math.sin(angle) * radius + planetPosition[2]  // Z-axis
-
-            // Point the satellite towards the center of the orbit and then rotate it to face away
+            sattelliteRef.current.position.x = Math.cos(angle) * radius + planetPosition[0]
+            sattelliteRef.current.position.y = planetPosition[1]
+            sattelliteRef.current.position.z = Math.sin(angle) * radius + planetPosition[2]
             sattelliteRef.current.lookAt(...planetPosition)
-            sattelliteRef.current.rotation.y += Math.PI // Rotate 180 degrees to face away from the planet
+            sattelliteRef.current.rotation.y += Math.PI
         }
     })
 
@@ -66,7 +61,6 @@ const Satellite = ({ onLoad }) => {
             >
                 <primitive object={scene} />
             </mesh>
-            {/* Adjusted PointLight position and properties */}
             <pointLight ref={lightRef} position={[-6, 1, -1]} intensity={10} distance={15} decay={1} />
         </>
     )
